871ff45232c144a676a67ae7d7d6911f528047b5
benscott.co.uk <ben@benscott.co.uk>
2019-01-22 17:10:25 +0000

    OO approach

---
+++ info-box.js
@@ -0,0 +1,9 @@
+function InfoBox() {
+
+
+    this.draw = function () {
+
+    }
+
+
+}
---
+++ stacked-bar-chart.js
@@ -0,0 +1,144 @@
+function StackedBarChart(svg, innerRadius) {
+
+    var width = +svg.attr("width");
+    var height = +svg.attr("height");
+    var outerRadius = Math.min(width, height) * 0.35;
+
+    g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height * 0.5 + ")");
+
+    var x = d3.scaleBand()
+        .range([0, 2 * Math.PI])
+        .align(0);
+
+    var y = d3.scaleRadial()
+        .range([innerRadius, outerRadius]);
+
+    var z = d3.scaleOrdinal()
+        .range(["#9D7800", "#955C00", "#8A3C09", "#7A0022", '#800023', "#390E2D"]);
+
+    this.draw = function () {
+
+        var data = []
+        data.columns = dataset.explainedByColumns;
+
+        dataset.forEach(function (d) {
+            var obj = d.properties;
+            obj['id'] = d.id;
+            data.push(obj)
+        });
+
+        console.log(data);
+        // var countryHappiness;
+        // if (typeof (d.properties['happiness']) !== 'undefined') {
+        //     countryHappiness = d.properties['happiness'];
+        //     for (i = 0, total = 0; i < data.columns.length; ++i) {
+        //         var column = data.columns[i];
+        //         total += parseFloat(countryHappiness[column]);
+        //     }
+        //     // The explained total doesn't always match the happiness score
+        //     // So bump it up a bit so it does
+        //     if (total != countryHappiness['Happiness score']) {
+        //         var diffFactor = countryHappiness['Happiness score'] / total
+        //         for (i = 0, total = 0; i < data.columns.length; ++i) {
+        //             var column = data.columns[i];
+        //             countryHappiness[column] *= diffFactor
+        //         }
+        //     }
+
+        //     countryHappiness['total'] = countryHappiness['Happiness score'];
+        //     countryHappiness['name'] = d.properties['name'];
+        //     countryHappiness['id'] = d.id;
+        //     countryHappiness['centroid'] = d3.geoCentroid(d);
+
+
+        //     data.push(countryHappiness)
+        // }
+
+
+        // data.sort(function (a, b) { return b.total - a.total; });
+
+        x.domain(data.map(function (d) { return d['name']; }));
+
+        y.domain([0, d3.max(data, function (d) { return d.total; })]);
+
+        z.domain(data.columns);
+
+        var stack = d3.stack()
+            .keys(data.columns)
+
+        g.append("g")
+            .selectAll("g")
+            .data(stack(data))
+            .enter().append("g")
+            .attr("fill", function (d) {
+                return z(d.key);
+            })
+            .selectAll("path")
+            .data(function (d) { return d; })
+            .enter().append("path")
+            .attr("class", function (d) {
+                return 'country-' + d.data.id
+            })
+            .on("mouseover", function (d) {
+                rotateMapToCentroid(d.data.centroid)
+                highlightCountry(d.data)
+            })
+            .on("mouseout", function (d) {
+                unHighlightCountry(d.data)
+            })
+            .on("click", function (d) {
+                showCountryInfo(d.data)
+            })
+            .attr("d", d3.arc()
+                .innerRadius(function (d) {
+                    return y(d[0]);
+                })
+                .outerRadius(function (d) {
+                    return y(d[1]);
+                })
+                .startAngle(function (d) {
+                    return x(d.data['name']);
+                })
+                .endAngle(function (d) {
+                    return x(d.data['name']) + x.bandwidth();
+                })
+                .padAngle(0.01)
+                .padRadius(innerRadius)
+            );
+
+        outerRadius += 10
+
+        var label = g.append("g")
+            .selectAll("g")
+            .data(data)
+            .enter().append("g")
+            .attr("text-anchor", "start")
+            .attr("class", function (d) {
+                return 'label country-' + d.id
+            })
+            .attr("transform", function (d) {
+                return "rotate(" + ((x(d.name) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")translate(" + outerRadius + ",0)";
+            })
+            .on("mouseover", function (d) {
+                rotateMapToCentroid(d.centroid)
+                highlightCountry(d)
+            })
+            .on("mouseout", function (d) {
+                unHighlightCountry(d)
+            })
+            .on("click", function (d) {
+                showCountryInfo(d)
+            })
+
+        label.append("text")
+            .attr("transform", function (d) {
+                return (x(d.name) + x.bandwidth() / 2 + Math.PI / 2) % (2 * Math.PI) < Math.PI ? "rotate(0)translate(0,5)" : "rotate(0)translate(0,5)";
+            })
+            .text(function (d) { return d.name; })
+
+
+
+    }
+
+
+}
