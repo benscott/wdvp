ef26b9ee6f4dc64145f855cf36b68caf35794157
benscott.co.uk <ben@benscott.co.uk>
2019-01-24 18:50:00 +0000

    All working

--- stacked-bar-chart.js
+++ stacked-bar-chart.js
@@ -9,7 +9,7 @@ function StackedBarChart(svg, innerRadius) {
     self.data = [];
     self.filters = [];
 
-    g = svg.append("g").attr("transform", "translate(" + self.width / 2 + "," + self.height * 0.5 + ")");
+    self.g = svg.append("g").attr("transform", "translate(" + self.width / 2 + "," + self.height * 0.5 + ")");
 
     self.x = d3.scaleBand()
         .range([0, 2 * Math.PI])
@@ -74,11 +74,11 @@ function StackedBarChart(svg, innerRadius) {
 
         self.addLegend();
 
-        g.selectAll("g.bar-group")
+        self.g.selectAll("g.bar-group")
             .data(stack(self.data))
             .enter().append("g")
             .attr("fill", function (d) {
-                return;
+                return 'red';
             })
             .attr("class", function (d) {
                 return 'bar-group ' + self.z(d.key)
@@ -115,10 +115,11 @@ function StackedBarChart(svg, innerRadius) {
 
         var labelRadius = self.outerRadius + 10
 
-        var label = g.append("g")
-            .selectAll("g")
+        var label = self.g.append("g")
+            .selectAll("g.label")
             .data(self.data)
-            .enter().append("g")
+            .enter()
+            .append("g")
             .attr("text-anchor", "start")
             .attr("class", function (d) {
                 return 'label country-' + d.id
@@ -159,98 +160,222 @@ function StackedBarChart(svg, innerRadius) {
 
         var transition = d3.transition().duration(200);
 
-        // var sort = function (a, b) {
-        //     return a['GDP per capita'] - b['GDP per capita']
+        var sortColumns = []
+        self.data.columns.forEach(function (column) {
+            if (self.filters.indexOf(column) == -1) {
+                sortColumns.push(column)
+            }
 
-
-
-        //     // return d3.descending(function (a) {
-        //     //     console.log(a)
-        //     //     return a.happinessScore
-        //     // }, function (b) {
-        //     //     return b.happinessScore
-        //     // });
-        // }
+        });
+        self.data.sort(function (a, b) {
+            aTotal = 0
+            bTotal = 0
+            sortColumns.forEach(function (column) {
+                aTotal += a[column]
+                bTotal += b[column]
+            });
+            return d3.descending(aTotal, bTotal)
+        });
 
         // Create a copy. Is this really the best way of doing this!?!
         var dataCopy = JSON.parse(JSON.stringify(self.data));
 
+        // If a filter has been applied, hide the bar by setting length to zero
         dataCopy.forEach(function (d, i) {
             self.filters.forEach(function (filter) {
                 d[filter] = 0
             });
         })
 
-        self.data.sort(function (a, b) {
-            var aTotal = a['happinessScore'];
-            var bTotal = b['happinessScore'];
-            console.log(aTotal)
-            self.filters.forEach(function (filter) {
-                aTotal -= a[filter];
-                bTotal -= b[filter]
-            });
-            return bTotal - aTotal
-        });
 
-        self.x.domain(self.data.map(function (d) { return d['name']; }));
+        console.log(sortColumns);
+
+        // Sort the data by subtracting
+        // dataCopy.sort(function (a, b) {
+        //     var aTotal = a['happinessScore'];
+        //     var bTotal = b['happinessScore'];
+        //     self.filters.forEach(function (filter) {
+        //         aTotal -= a[filter];
+        //         bTotal -= b[filter]
+        //     });
+        //     d3.descending(aTotal, bTotal)
+        //     // return bTotal - aTotal
+        // });
+
+
+
+        var item = self.data.columns[Math.floor(Math.random() * self.data.columns.length)];
+
+        console.log(item)
+
+
+
+
+
+        // var sort = function (a, b) {
+        //     return d3.descending(a['GDP per capita'], b['GDP per capita'])
+        // };
+
+        self.x.domain(dataCopy.map(function (d) { return d['name']; }));
+        self.y.domain([0, d3.max(dataCopy, function (d) { return d.happinessScore; })]);
 
         var stack = d3.stack().keys(self.data.columns)
 
-        var groups = g.selectAll("g.bar-group")
+        // transition.selectAll("g.bar-group")
+        //     .data(stack(dataCopy))
+        //     .selectAll("path.bar")
+        //     .data(function (d) { return d; })
+
+        self.g.selectAll("g.bar-group")
             .data(stack(dataCopy))
+            .selectAll("path.bar")
+            .data(function (d) { return d; })
+            .attr("class", function (d) {
+                return 'bar country-' + d.data.id;
+            })
+
+        self.g.selectAll("g.bar-group")
+            .selectAll(".bar")
+            .transition()
+            .duration(20)
+            .attr("d", d3.arc()
+                .innerRadius(function (d) {
+                    return self.y(d[0]);
+                })
+                .outerRadius(function (d) {
+                    return self.y(d[1]);
+                })
+                .startAngle(function (d) {
+                    return self.x(d.data['name']);
+                })
+                .endAngle(function (d) {
+                    return self.x(d.data['name']) + self.x.bandwidth();
+                })
+                .padAngle(0.01)
+                .padRadius(innerRadius)
+            )
+
+
+        // .on("mouseover", function (d) {
+        //     console.log(d);
+        //     // rotateMapToCentroid(d.data.centroid)
+        //     highlightCountry(d.data)
+        // })
+        // .on("mouseout", function (d) {
+        //     unHighlightCountry(d.data)
+        // })
 
         // Reattach the events
-        groups.selectAll(".bar")
-            .on("mouseover", function (d) {
-                rotateMapToCentroid(d.data.centroid)
-                highlightCountry(d.data)
-            })
-            .on("mouseout", function (d) {
-                unHighlightCountry(d.data)
-            })
-            .data(function (d) { return d; })
+        // groups.selectAll(".bar")
+        //     .data(function (d) { return d; })
+        //     .on("mouseover", function (d) {
+        //         rotateMapToCentroid(d.data.centroid)
+        //         highlightCountry(d.data)
+        //     })
+        //     .on("mouseout", function (d) {
+        //         unHighlightCountry(d.data)
+        //     })
+        //     .data(function (d) { return d; })
+
+        // transition.selectAll("g.bar-group")
+        //     .selectAll(".bar")
+        //     // .data(function (d) { return d; })
+        //     .attr("d", d3.arc()
+        //         .innerRadius(function (d) {
+        //             return self.y(d[0]);
+        //         })
+        //         .outerRadius(function (d) {
+        //             return self.y(d[1]);
+        //         })
+        //         .startAngle(function (d) {
+        //             return self.x(d.data['name']);
+        //         })
+        //         .endAngle(function (d) {
+        //             return self.x(d.data['name']) + self.x.bandwidth();
+        //         })
+        //         .padAngle(0.01)
+        //         .padRadius(innerRadius)
+        //     );
 
 
-        transition.selectAll("g.bar-group").each(function (group, i) {
-            d3.select(this).selectAll(".bar")
-                .attr("d", d3.arc()
-                    .innerRadius(function (d) {
-                        return self.y(d[0]);
-                    })
-                    .outerRadius(function (d) {
-                        return self.y(d[1]);
-                    })
-                    .startAngle(function (d) {
-                        return self.x(d.data['name']);
-                    })
-                    .endAngle(function (d) {
-                        return self.x(d.data['name']) + self.x.bandwidth();
-                    })
-                    .padAngle(0.01)
-                    .padRadius(innerRadius)
-                );
-        })
+        // transition.selectAll("g.bar-group").each(function (group, i) {
+        //     d3.select(this).selectAll(".bar")
+        //         .data(function (d) { return d; })
+        //         .attr("d", d3.arc()
+        //             .innerRadius(function (d) {
+        //                 return self.y(d[0]);
+        //             })
+        //             .outerRadius(function (d) {
+        //                 return self.y(d[1]);
+        //             })
+        //             .startAngle(function (d) {
+        //                 return self.x(d.data['name']);
+        //             })
+        //             .endAngle(function (d) {
+        //                 return self.x(d.data['name']) + self.x.bandwidth();
+        //             })
+        //             .padAngle(0.01)
+        //             .padRadius(innerRadius)
+        //         );
+        // })
 
-        
-
-        var label = g.append("g")
-            .selectAll("g")
-            .data(self.data)
-            .enter().append("g")
-            .attr("text-anchor", "start")
+        self.g.selectAll("g.label")
+            .data(dataCopy)
             .attr("class", function (d) {
                 return 'label country-' + d.id
             })
+            .select('text')
+            .text(function (d) { return d.name; })
             .attr("transform", function (d) {
-                return "rotate(" + ((self.x(d.name) + self.x.bandwidth() / 2) * 180 / Math.PI - 90) + ")translate(" + labelRadius + ",0)";
+                return (self.x(d.name) + self.x.bandwidth() / 2 + Math.PI / 2) % (2 * Math.PI) < Math.PI ? "rotate(0)translate(0,5)" : "rotate(0)translate(0,5)";
             })
-            .on("mouseover", function (d) {
-                rotateMapToCentroid(d.centroid)
-                highlightCountry(d)
-            })
-            .on("mouseout", function (d) {
-                unHighlightCountry(d)
-            })
+
+
+        // label.selectAll('text')
+        //     .data(dataCopy)
+
+
+
+        // .data(function (d) { return d; })
+
+        // transition.selectAll("g.label")
+        //     .selectAll('text')
+
+
+        //     .text(function (d) { return d.name; })
+
+
+        // .text(function (d) { return d.name; })
+
+        // console.log(label.selectAll('text'));
+
+
+        // label.append("text")
+        //     .attr("transform", function (d) {
+        //         return (self.x(d.name) + self.x.bandwidth() / 2 + Math.PI / 2) % (2 * Math.PI) < Math.PI ? "rotate(0)translate(0,5)" : "rotate(0)translate(0,5)";
+        //     })
+        // .text(function (d) { return d.name; })
+
+
+
+        // var label = g.append("g")
+        //     .selectAll("g")
+        //     .data(self.data)
+        //     .enter().append("g")
+        //     .attr("text-anchor", "start")
+        //     .attr("class", function (d) {
+        //         return 'label country-' + d.id
+        //     })
+        //     .attr("transform", function (d) {
+        //         return "rotate(" + ((self.x(d.name) + self.x.bandwidth() / 2) * 180 / Math.PI - 90) + ")translate(" + labelRadius + ",0)";
+        //     })
+        //     .on("mouseover", function (d) {
+        //         rotateMapToCentroid(d.centroid)
+        //         highlightCountry(d)
+        //     })
+        //     .on("mouseout", function (d) {
+        //         unHighlightCountry(d)
+        //     })
         // console.log(group)
         // console.log(d3.select(group).selectAll(".bar"))
         // group.selectAll(".bar")
